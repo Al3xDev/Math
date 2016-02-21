5954d55ad7ca91f153e829079b54a8de28c9aad4
Stoica Alexandru Gabriel <capitanulrex98@yahoo.com>
2016-02-21 09:54:37 +0200

    Update CoordSystem.cpp

--- CoordSystem.cpp
+++ CoordSystem.cpp
@@ -39,3 +39,66 @@ Vector3f SphericalCoordf::GetXYZ()
 	temp.z=cos(beta)*r;
 	return temp;
 }
+
+
+SphericalCoordd::SphericalCoordd()	:
+	r(1), alfa(0), beta(0) {}
+SphericalCoordd::SphericalCoordd(double _r, double _alfa, double _beta) :
+	r(_r), alfa(_alfa), beta(_beta) {}
+SphericalCoordd::SphericalCoordd(SphericalCoordd& _SphericalCoordf)
+{
+	r=_SphericalCoordf.r;
+	alfa=_SphericalCoordf.alfa;
+	beta=_SphericalCoordf.beta;
+}
+
+double SphericalCoordd::GetR() { return r; }
+double SphericalCoordd::GetAlfa() { return alfa; }
+double SphericalCoordd::GetBeta() { return beta; }
+void SphericalCoordd::SetR(double _r) { r=_r; }
+void SphericalCoordd::SetAlfa(double _alfa) { alfa=_alfa; }
+void SphericalCoordd::SetBeta(double _beta) { beta=_beta; }
+
+double SphericalCoordd::GetX()
+{
+	return sin(beta)*cos(alfa)*r;
+}
+double SphericalCoordd::GetY()
+{
+	return sin(beta)*sin(alfa)*r;
+}
+double SphericalCoordd::GetZ()
+{
+	return cos(beta)*r;
+}
+Vector3f SphericalCoordd::GetXYZ()
+{
+	Vector3f temp;
+	temp.x=sin(beta)*cos(alfa)*r;
+	temp.y=sin(beta)*sin(alfa)*r;
+	temp.z=cos(beta)*r;
+	return temp;
+}
+
+PolarCoord::PolarCoord():
+	r(1.0), alfa(0.0) {}
+PolarCoord::PolarCoord(float _r, float _alfa):
+	r(_r), alfa(_alfa) {}
+PolarCoord::PolarCoord(PolarCoord& _PolarCoord):
+	r(_PolarCoord.r), alfa(_PolarCoord.alfa) {}
+
+float PolarCoord::GetR() { return r; }
+float PolarCoord::GetAlfa() { return alfa; }
+
+void PolarCoord::SetR(float _r) { r=_r; }
+void PolarCoord::SetAlfa(float _alfa) { alfa=_alfa; }
+
+float PolarCoord::GetX() { return cos(alfa)*r; }
+float PolarCoord::GetY() { return sin(alfa)*r; }
+Vector2f PolarCoord::GetXY()
+{
+	Vector2f temp;
+	temp.x=r*cos(alfa);
+	temp.y=r*sin(alfa);
+	return temp;
+}
