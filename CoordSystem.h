9fcd311837e264aef3b3f7591e56d96662b91d12
Stoica Alexandru Gabriel <capitanulrex98@yahoo.com>
2016-03-03 19:21:58 +0200

    Update CoordSystem.h

--- CoordSystem.h
+++ CoordSystem.h
@@ -1,10 +1,15 @@
 #ifndef COORD_SYSTEM_H
 #define COORD_SYSTEM_H
+#include "../Vector/VectorF.h"
 
+///2D coord - 2*Floats
 class PolarCoord
 {
 public:
-    float r,alfa;
+    ///distance from origin
+    float r;
+    ///angle (r,Ox)
+    float alfa;
 	PolarCoord();
 	PolarCoord(float,float);
 	PolarCoord(PolarCoord&);
@@ -12,6 +17,7 @@ public:
 	inline float GetR();
 	inline float GetAlfa();
 
+	inline void Set(PolarCoord&);
 	inline void SetR(float);
 	inline void SetAlfa(float);
 
@@ -21,35 +27,49 @@ public:
 	Vector2f GetXY();
 };
 
-
+/// 3D Coord System - 3*Floats
 class CylindricalCoord
 {
 public:
-	float r,alfa,z;
-	CylindricalCoord();
+	///distande from origin
+	float r;
+    ///angle (r,Ox)
+    float alfa;
+    ///z coord
+    float z;
+
+    CylindricalCoord();
 	CylindricalCoord(float,float,float);
 	CylindricalCoord(CylindricalCoord&);
 
 	inline float GetR();
 	inline float GetAlfa();
-	inline float GetZ();
+	inline void Set(CylindricalCoord&);
 	inline void SetR(float);
 	inline void SetAlfa(float);
 	inline void SetZ(float);
 
 	///Cartesian Coord
+
 	float GetX();
 	float GetY();
+	float GetZ();
 	Vector3f GetXYZ();
 };
 
 
 
-/// 3D Coord System
+/// 3D Coord System - 3*Floats
 class SphericalCoordf
 {
 public:
-	float r,alfa,beta;
+	///distance from origin
+	float r;
+    ///angle (r,Ox)
+    float alfa;
+    ///angle (r,Oz)
+    float beta;
+
 	SphericalCoordf();
 	SphericalCoordf(float,float,float);
 	SphericalCoordf(SphericalCoordf&);
@@ -57,21 +77,31 @@ public:
 	inline float GetR();
 	inline float GetAlfa();
 	inline float GetBeta();
+
+	inline void Set(SphericalCoordf&);
 	inline void SetR(float);
 	inline void SetAlfa(float);
 	inline void SetBeta(float);
 
 	///Cartesian Coord
+
 	float GetX();
 	float GetY();
 	float GetZ();
 	Vector3f GetXYZ();
 };
 
+/// 3D Coord System - 3*Double
 class SphericalCoordd
 {
 public:
-	double r,alfa,beta;
+	///distance from origin
+	double r;
+    ///angle (r,Ox)
+    double alfa;
+    ///angle (r,Oz)
+    double beta;
+
 	SphericalCoordd();
 	SphericalCoordd(double,double,double);
 	SphericalCoordd(SphericalCoordd&);
@@ -79,15 +109,22 @@ public:
 	inline double GetR();
     inline double GetAlfa();
 	inline double GetBeta();
+
+	inline void Set(SphericalCoordd&);
 	inline void SetR(double);
 	inline void SetAlfa(double);
 	inline void SetBeta(double);
 
 	///Cartesian Coord
+
     double GetX();
     double GetY();
     double GetZ();
-    Vector3f GetXYZ();
+    ///Vector3d GetXYZ();
+};
+
+
+#endif
 };
 
 
